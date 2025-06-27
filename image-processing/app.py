43c0043b28198aee4a7c885832c038ffc563c3f6
Leonardo Madeira <lnrdmadeira@gmail.com>
2025-06-27 02:11:55 -0300

    feat: imagens-redmencionadas

---
+++ image-processing/app.py
@@ -0,0 +1,37 @@
+'''
+sair de x para (128px,72px)
+'''
+
+import cv2
+import os
+
+def processingImage(pasta_origem, pasta_destino):
+    novo_tamanho = (256, 144)
+
+    for nome_arquivo in os.listdir(pasta_origem):
+        caminho_arquivo = os.path.join(pasta_origem, nome_arquivo)
+
+        imagem = cv2.imread(caminho_arquivo)
+
+        if imagem is not None:
+            imagem_redimensionada = cv2.resize(imagem, novo_tamanho)
+
+            caminho_salvar = os.path.join(pasta_destino, nome_arquivo)
+            cv2.imwrite(caminho_salvar, imagem_redimensionada)
+
+            print(f"Imagem salva: {nome_arquivo}")
+        else:
+            print(f"Falha ao abrir: {nome_arquivo}")
+
+origem_jumping = "./frames-capture/jumping-tratado"
+destino_jumping = "./image-processing/jumping-redimensionado"
+
+origem_nojumping = "./frames-capture/no-jumping-tratado"
+destino_nojumping = "./image-processing/no-jumping-redimensionado"
+
+os.makedirs(destino_jumping, exist_ok=True)
+os.makedirs(destino_nojumping, exist_ok=True)
+
+processingImage(origem_jumping, destino_jumping)
+processingImage(origem_nojumping, destino_nojumping)
+

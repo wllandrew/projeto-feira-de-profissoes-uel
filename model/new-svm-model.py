d0924b6a7b37616f080353c0e2be5851f32dc356
kf-martins <kaua.felipe.martins@uel.br>
2025-06-28 06:00:37 -0300

    raw: generating new svm model

--- model/new-svm-model.py
+++ model/new-svm-model.py
@@ -4,26 +4,33 @@ import pandas as pd
 import os
 import joblib
 
-DATASET_PATH = os.path.join(os.getcwd(), 'jumping-dataset.csv')
+DATASET_PATH = os.path.join(os.getcwd(), 'dataset/dataset.csv')
 
 csv = pd.read_csv(DATASET_PATH)
 
 y = csv["Jumping"]
 x = csv.drop(columns=["Jumping"])
 
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
+print('Separando dataset...')
+X_train, X_test, y_train, y_test = train_test_split(x.to_numpy(), y.to_numpy(), test_size=0.2)
+print('Dataset pronto!')
 
-model = svm.SVC(kernel='rbf', gamma='auto', probability=True)
-model.fit(x_train, y_train)
 
-score = model.score(x_test, y_test)
-probabilidades = model.predict_proba(x_test)
+model = svm.SVC(kernel='rbf', probability=True)
+# model = svm.SVC(kernel='linear', probability=True)
+print('Treinando modelo...')
+model.fit(X_train, y_train)
+print('Modelo treinado!')
+
+print('Analisando acurácia...')
+score = model.score(X_test, y_test)
+probabilidades = model.predict_proba(X_test)
 
 for p in probabilidades:
     print(f"Não pulando: {p[0]*100:.1f}% | Pulando: {p[1]*100:.1f}%")
 
 print(f'Score: ', score)
 
-path_model = 'jumping-svm-model.pkl'
+path_model = 'models/jumping-svm-model.pkl'
 joblib.dump(model, path_model)
 print(f"Modelo salvo como {path_model}")

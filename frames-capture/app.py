5421cd24e5a2dacfc0f88a794c7e33a6ee42c4b0
Leonardo Madeira <lnrdmadeira@gmail.com>
2025-06-28 18:57:52 -0300

    row: gerando novo arquivo - Novo_dataset

--- frames-capture/app.py
+++ frames-capture/app.py
@@ -6,6 +6,7 @@ import os
 cap = cv2.VideoCapture(2)
 
 LINE_Y = 50 
+LINE_MINY = 100
 
 success, img = cap.read()
 first_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -18,6 +19,7 @@ while True:
     movimento_detectado = False
     
     LINE_Y = max(0, min(LINE_Y, img.shape[0] - 1))
+    LINE_MINY = max(0, min(LINE_MINY, img.shape[0] - 1))
     key = cv2.waitKey(30) & 0xFF
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -31,6 +33,8 @@ while True:
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     cv2.line(img, (0, LINE_Y), (img.shape[1], LINE_Y), (0, 255, 255), 2)
+    cv2.line(img, (0, LINE_MINY), (img.shape[1], LINE_MINY), (255, 0, 0), 2)
+
 
     movimento_detectado = False
 
@@ -41,7 +45,7 @@ while True:
         (x, y, w, h) = cv2.boundingRect(contour)
         # center_y = y + h // 2
         center_y = y
-        timestamp = int(time.time())
+        timestamp = int(time.time() * 10)
         
         if center_y <= LINE_Y:
            
@@ -58,7 +62,8 @@ while True:
             print(f"[INFO] Movimento detectado! Imagem salva: captura_{timestamp}.jpg")
             movimento_detectado = True
         
-        elif timestamp % 2 == 0: 
+        elif timestamp % 2 == 0 and center_y >= LINE_MINY:
+        
             
             name_file = f"not_jumping_{timestamp}.jpg"
             local_dir = os.path.dirname(os.path.abspath(__file__))
@@ -82,6 +87,10 @@ while True:
         LINE_Y += 10
     elif key == 119:  # seta para cima
         LINE_Y -= 10
+    elif key == 101:
+        LINE_MINY -= 10
+    elif key == 100:
+        LINE_MINY += 10
     elif key == 27:  # ESC
         break
 

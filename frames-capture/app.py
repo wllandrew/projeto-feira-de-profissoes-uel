8169944ad3690d3a31c0230bf8b38d20ebf8ddea
kf-martins <kaua.felipe.martins@uel.br>
2025-06-28 21:12:04 -0300

    refactor: frames-capture -> processing image functions and optimalized lines verification

--- frames-capture/app.py
+++ frames-capture/app.py
@@ -13,6 +13,13 @@ def saveImageJPG(directory, image, file_name):
 
     print(f"[INFO] Imagem salva: {file}")
 
+def processImageDfContours(img):
+    frame_delta = cv2.absdiff(first_gray, gray)
+    thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
 cap = cv2.VideoCapture(0)
 
 LINE_Y = 50 
@@ -36,35 +43,35 @@ while True:
     cv2.line(img, (0, LINE_MINY), (img.shape[1], LINE_MINY), (255, 0, 0), 2)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    save_img = cv2.resize(gray, (128,128))
-    save_img = cv2.Canny(save_img, 155,105)
 
-    frame_delta = cv2.absdiff(first_gray, gray)
-    thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    for contour in contours:
-        if cv2.contourArea(contour) < 1000:
-            continue
+    if capturing:
+        save_img = cv2.resize(gray, (128,128))
+        save_img = cv2.Canny(save_img, 155,105)
 
-        (x, y, w, h) = cv2.boundingRect(contour)
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        contours = processImageDfContours(gray)
+        contours_up = False
 
-        if capturing:
+        for contour in contours:
+            if cv2.contourArea(contour) < 1000:
+                continue
+
             timestamp = int(time.time())
+
+            (x, y, w, h) = cv2.boundingRect(contour)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             if y <= LINE_Y: # Passou da linha de pulo
                 saveImageJPG('jumping', save_img, f'jumping_{timestamp}')
                 movimento_detectado = True
-            
-            elif timestamp % 2 == 0 and y >= LINE_MINY: # não passou da linha mínima
+            elif y <= LINE_MINY: # passou da linha mínima
+                contours_up = True
+            elif timestamp % 2 == 0 and not contours_up: # não passou da linha mínima
                 saveImageJPG('not-jumping', save_img, f'not_jumping_{timestamp}')
 
     if not movimento_detectado:
         first_gray = gray.copy()
 
-    cv2.imshow("Detecção de Movimento", img)
+    cv2.imshow("Detectando Movimento", img)
 
     if key == ord('p'):
         capturing = not capturing
